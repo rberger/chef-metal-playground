8a32451ce0bd7a3faed4c661fd8ef1e2a0e97d3b
Robert J. Berger <rberger@ibd.com>
2014-04-23 18:45:38 -0700

    Started Readme

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,15 +1,16 @@
 current_dir = File.dirname(__FILE__)
 user = ENV['OPSCODE_USER'] || ENV['USER']
 orgname = ENV['ORGNAME'] || "chef"
+client_name = ENV['HOSTNAME']
+chef_server_url          "https://192.168.67.250"
 
 node_name                user
-client_key               "#{ENV['HOME']}/.chef/cymek.local/#{user}.pem"
+client_key               "#{ENV['HOME']}/.chef/#{client_name}/#{user}.pem"
 validation_client_name   "#{orgname}-validator"
-validation_key           "#{ENV['HOME']}/.chef/cymek.local/#{orgname}-validator.pem"
-chef_server_url          "https://192.168.67.250"
+validation_key           "#{ENV['HOME']}/.chef/#{client_name}/#{orgname}-validator.pem"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntax_check_cache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
-cookbook_copyright "Rivermeadow.com"
+cookbook_copyright "Robert J. Berger"
 cookbook_license "apachev2"
 cookbook_email "rberger@ibd.com"
 
